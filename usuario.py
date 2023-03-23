bb919c719496a1ad82d69b5740eec05dca780aba
DiegoOpazoD <diegoopazod@gmail.com>
2023-03-22 22:50:21 -0300

    hola

--- usuario.py
+++ usuario.py
@@ -2,7 +2,7 @@ class Usuario:
     def __init__(self, username, password, email):
         self.username = username
         self.password = password
-        self.email = email
+        self.email = email #hola
     
 
     def agregarTarea(self, tarea):
